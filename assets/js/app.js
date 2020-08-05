9e50a71690047be08f5ce067c622eca211ad2c11
ujw0l <bktujwol@gmail.com>
2020-08-05 18:45:46 +0545

    Sleeker look progress

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,25 +7,25 @@ window.addEventListener('load',()=> {
 
     let  bodyZone =  document.querySelector('body');
     bodyZone.addEventListener("dragover",(event)=>event.preventDefault());
-
+    bodyZone.style =`height:${window.innerHeight}px;width:${window.innerWidth}px;`;
     bodyZone.addEventListener('drop',(event)=>{
-    
         event.preventDefault();
         let img = event.dataTransfer.files[0]
-       
        if(FileReader){
-                
            let reader =  new FileReader();
-
            reader.readAsDataURL(img);
            reader.addEventListener('load',event=>{ 
-
             let dropedImg = new Image();
               dropedImg.src = event.target.result;
               dropedImg.addEventListener('load', event=>  jCrop.createOverlay(event.target));
            });
               
        }
+    })
+
+
+    window.addEventListener('resize',()=>{
+        bodyZone.style =`height:${window.innerHeight}px;width:${window.innerWidth}px;`;
     })
 
 });
